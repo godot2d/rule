06edd5412530eb70c8c6e8cebf0323cfa0869ffc
godot2d <947863080@qq.com>
2026-03-11 14:25:11 +0800

    update

--- 中转.js
+++ 中转.js
@@ -1,13 +1,10 @@
-
-
 function operator(proxies, targetPlatform, context) {
 
   return proxies.map(proxy => {
 
-    // 检查 server 是否是纯 IP
-    if (/^\d+\.\d+\.\d+\.\d+$/.test(proxy.name) || /^\d+\.\d+\.\d+\.\d+$/.test(proxy.server)) {
-      proxy.override = proxy.override || {}
-      proxy.override["dialer-proxy"] = "🚀 节点选择"
+    // Name包含 ISP
+    if (/ISP/i.test(proxy.name)) {
+      proxy["dialer-proxy"] = "🇺🇲 美国节点"
     }
 
     return proxy
